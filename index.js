b80039ba47f1d876bed33795b6bc5643b25c5143
Manvi Tyagi <manvityagi770@gmail.com>
2020-08-20 13:57:53 +0530

    endpoints

--- index.js
+++ index.js
@@ -73,9 +73,6 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-// app.listen(process.env.PORT,process.env.IP,function(){
-//     console.log("The Yelpcamp server has started")
-// });
 
 const port = process.env.PORT || 3030;
 app.listen(port, function() {
