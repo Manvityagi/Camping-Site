c3d4a343ed80d51ed031a2438cdb5fd26627b3bf
Manvi Tyagi <manvityagi770@gmail.com>
2020-08-20 14:01:39 +0530

    linter

--- models/campground.js
+++ models/campground.js
@@ -1,25 +1,24 @@
-var mongoose=require("mongoose"); 
+var mongoose = require("mongoose");
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
-    name: String,
-    price: String,
-    image: String,
-    location: String,
-    description: String,
-    comments: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Comment"
-        }
-        
-        ],
-      author: {
-      id: {
-             type: mongoose.Schema.Types.ObjectId,
-             ref: "User"
-          },
-             username: String
-       }
+  name: String,
+  price: String,
+  image: String,
+  location: String,
+  description: String,
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    username: String,
+  },
 });
 
-module.exports = mongoose.model("Campground",campgroundSchema); 
+module.exports = mongoose.model("Campground", campgroundSchema);
--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 var mongoose = require("mongoose");
-var passportLocalMongoose = require("passport-local-mongoose")
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
+  username: String,
+  password: String,
 });
 
 UserSchema.plugin(passportLocalMongoose);
